1a7940cf23f4c2c99b96d5e1c186d0db00568f1e
coderdy-git <coderdy-git@users.noreply.github.com>
2026-06-14 18:50:26 +0700

    fix: add CORS middleware to allow frontend access

--- main.go
+++ main.go
@@ -472,6 +472,22 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	// Middleware CORS
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	rate := limiter.Rate{Period: 1 * time.Minute, Limit: 30}
 	limitMiddleware := mgin.NewMiddleware(limiter.New(memory.NewStore(), rate))
 	r.Use(limitMiddleware)
